f077963efb42fb00328d41bc8beffd605bad225b
Julio Santana <jucsantana@gmail.com>
2022-03-24 12:26:37 -0300

    first commit

---
+++ src/main/java/com/jcs/cursos/ApiCursoSpringBoot/ApiCursoSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.jcs.cursos.ApiCursoSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCursoSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCursoSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jcs/cursos/ApiCursoSpringBoot/ApiCursoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcs.cursos.ApiCursoSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCursoSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
